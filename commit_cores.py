4fd990288f958c3e90ca6187de34c521df38eea8
ivan-paz <ivnpaz@gmail.com>
2018-12-03 22:13:20 +0100

    ruler

--- commit_cores.py
+++ commit_cores.py
@@ -1,14 +1,33 @@
+#         import the necessary functions
+#
 from what_happens_when_the_combination_arrives import what_happends_when_combination_arrives
 from optimum_partition_different_class import adjust_cores
-#
-#
-#
+from ruler import ruleExtraction
+#-----------------------------------------------------------------------------
+
+
+#function that converts an instance into set format
 def instance_to_set_format(instance):
     set_format = []
     for i in range( len(instance)-1 ):
         set_format.append(set([instance[i]]))
     set_format.append( instance[-1] )
     return set_format
+#break the affected cores into pieces
+import itertools
+import copy
+#rule = [1, [2,3], 'A']
+rule = [[1,4], [5,6], 'B']
+def expand_rule(rule):
+    rule = copy.deepcopy(rule)
+    expanded_rule = []
+    for i in range(len(rule)):
+        if type(rule[i]) != list:
+            rule[i] = [rule[i]]
+    for i in itertools.product(*rule):
+        expanded_rule.append(i)
+    return expanded_rule
+print(expand_rule(rule))
 
 
 def commit_cores(new_instance,cores):
@@ -21,6 +40,11 @@ def commit_cores(new_instance,cores):
     
     if case == 'same class':
         print('same class')
+        #break the rules
+        #call ruler
+        cores.append(instance_to_set_format(new_instance))
+        print(ruleExtraction(cores,1,0))
+        #solve the contradictions
 
     elif case == 'different class':
         print('different class')
@@ -29,21 +53,24 @@ def commit_cores(new_instance,cores):
     else:
         print('nothing')
         cores.append(instance_to_set_format(new_instance))
-    print(cores)
-
-
+    print('final',cores)
 
 #combination = (3, 7, 'B') 
 #cores = []
 #commit_cores(combination,cores)
 
-#same class
-combination = (3, 7, 'B') 
-cores = [  [{1,4}, {6,8}, 'A']  ]
-commit_cores(combination,cores)
+#different class
+#combination = (3, 7, 'B') 
+#cores = [  [{1,4}, {6,8}, 'A']  ]
+#commit_cores(combination,cores)
 
 #nothing
-combination = (5, 7, 'B') 
-cores = [  [{1,4}, {6,8}, 'A']  ]
-commit_cores(combination,cores)
+#combination = (5, 7, 'B') 
+#cores = [  [{1,4}, {6,8}, 'A']  ]
+#commit_cores(combination,cores)
+
+# same class
+#combination = (3, 6, 'A') 
+#cores = [  [{1,4}, {6,8}, 'A']  ]
+#commit_cores(combination,cores)
 
